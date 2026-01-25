da760c19fb9a69d29b5ae97813c662cec49c8a3f
Ryan Hogan <ryan_hogan2@aol.com>
2026-01-24 22:45:10 -0500

    fixed analytics

--- backend/app/analytics/__init__.py
+++ backend/app/analytics/__init__.py
@@ -1,3 +1,3 @@
-from .query_logger import log_user_query, get_query_stats
+from .query_logger import log_user_query, get_query_stats, get_daily_spend
 
-__all__ = ['log_user_query', 'get_query_stats']
+__all__ = ['log_user_query', 'get_query_stats', 'get_daily_spend']
